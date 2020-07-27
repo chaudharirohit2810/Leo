d6b0cf954f69ff9747f557b48dc071645d5e5005
chaudharirohit2810 <chaudharirohit2810@gmail.com>
2020-07-27 18:39:37 +0530

    feat: news offline support

--- app/src/main/java/com/rohit2810/leo_kotlin/database/NewsDao.kt
+++ app/src/main/java/com/rohit2810/leo_kotlin/database/NewsDao.kt
@@ -3,18 +3,22 @@ package com.rohit2810.leo_kotlin.database
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.rohit2810.leo_kotlin.models.Article
+import com.rohit2810.leo_kotlin.models.news.News
 
 @Dao
 interface NewsDao {
 
-    @Insert
-    suspend fun insertNews(article: Article)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNews(news: News)
 
-    @Query("DELETE FROM news_article_table")
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAllNews(news: List<News>)
+
+    @Query("DELETE FROM news_table")
     suspend fun deleteAllNews()
 
-    @Query("SELECT * FROM news_article_table")
-    fun getAllNews() : LiveData<List<Article>>
+    @Query("SELECT * FROM news_table")
+    fun getAllNews() : LiveData<List<News>>
 }
---
+++ app/src/main/java/com/rohit2810/leo_kotlin/database/NewsDateAdapter.kt
@@ -0,0 +1,30 @@
+package com.rohit2810.leo_kotlin.database
+
+import com.squareup.moshi.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+class CustomDateAdapter : JsonAdapter<Date>() {
+    private val dateFormat = SimpleDateFormat(SERVER_FORMAT, Locale.getDefault())
+
+    @FromJson
+    override fun fromJson(reader: JsonReader): Date? {
+        return try {
+            val dateAsString = reader.nextString()
+            dateFormat.parse(dateAsString)
+        } catch (e: Exception) {
+            null
+        }
+    }
+
+    @ToJson
+    override fun toJson(writer: JsonWriter, value: Date?) {
+        if (value != null) {
+            writer.value(value.toString())
+        }
+    }
+
+    companion object {
+        const val SERVER_FORMAT = ("yyyy-MM-dd'T'HH:mm:ss") // define your server format here
+    }
+}
